42e7cb11541905b517ef6c7abb3e986e0cf1425b
henkess <zinzerium@gmail.com>
2020-04-27 01:23:45 +0100

    random dice

--- random_dice.py
+++ random_dice.py
@@ -3,7 +3,9 @@ run = True
 while run:
     roll = random.randint(1, 6)
     rolled = input("press enter to continue, q to quit... ")
-    if rolled == "q": run = False
+    if rolled == "q": run = False       # you can use break
     print(roll)
+
+print("Thank you")
 
 
