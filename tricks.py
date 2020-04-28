d5c284a9fa2c83d7b7896b786cbe36717f5a7244
henkess <zinzerium@gmail.com>
2020-04-28 16:40:12 +0100

    some tricks

---
+++ tricks.py
@@ -0,0 +1,8 @@
+# create a list of numbers with comprehentions
+numbers = [n for n in range(1,10)]
+print(numbers)
+
+# list comrehention with condition
+numbers1 = [n for n in range(1,10) if n % 2 == 0]
+print(numbers1)
+
