043458403165dc241ca68f23c2f7175a2bd12380
henkess <zinzerium@gmail.com>
2020-04-29 00:50:06 +0100

    os module

---
+++ Os module.py
@@ -0,0 +1,59 @@
+import os
+import pwd
+# get the user ID
+os.getuid()
+
+# password module
+pwd.getpwuid(os.getuid())
+
+# Load all of the user data
+for id in pwd.getpwall():
+    print(id[0])
+
+# Executing a shell command
+os.system()
+
+# Get the users environment
+os.environ()
+
+#Returns the current working directory.
+os.getcwd()
+
+# Return the real group id of the current process.
+os.getgid()
+
+# Return the current process’s user id.
+os.getuid()
+
+# Returns the real process ID of the current process.
+os.getpid()
+
+# Set the current numeric umask and return the previous umask.
+os.umask(mask)
+
+# Return information identifying the current operating system.
+os.uname()
+
+# Change the root directory of the current process to path.
+os.chroot(path)
+
+# Return a list of the entries in the directory given by path.
+os.listdir(path)
+
+# Create a directory named path with numeric mode mode.
+os.mkdir(path)
+
+# Recursive directory creation function.
+os.makedirs(path)
+
+# Remove (delete) the file path.
+os.remove(path)
+
+# Remove directories recursively.
+os.removedirs(path)
+
+# Rename the file or directory src to dst.
+os.rename(src, dst)
+
+# Remove (delete) the directory path.
+os.rmdir(path)
