f3191ff74b84e26c133a94612e89982234bfba7c
henkess <zinzerium@gmail.com>
2020-04-29 16:46:46 +0100

    os module

--- tricks.py
+++ tricks.py
@@ -1,8 +1,21 @@
-# create a list of numbers with comprehentions
-numbers = [n for n in range(1,10)]
-print(numbers)
+# # create a list of numbers with comprehentions
+# numbers = [n for n in range(1,10)]
+# print(numbers)
 
-# list comrehention with condition
-numbers1 = [n for n in range(1,10) if n % 2 == 0]
-print(numbers1)
+# # list comrehention with condition
+# numbers1 = [n for n in range(1,10) if n % 2 == 0]
+# print(numbers1)
 
+# # comprehention dictionary
+# w = "mohammed ali"
+# lt = {l:w.count(l) for l in w if l !=' '}
+# print(lt)
+
+# # generator
+# num = (n for n in range(1,6))
+# print(num)
+# print(next(num))
+# print(next(num))
+# print("==================")
+# for n in num:
+#     print(n)
