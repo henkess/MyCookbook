a6d767c0f7d283c2ded3fe629c3528851f9232af
henkes <zinzerium@gmail.com>
2020-05-11 01:03:04 +0100

    regular expression

---
+++ regex.py
@@ -0,0 +1,16 @@
+# The group() method returns the part of the string where there is a match.
+
+import re
+
+string = '39801 356, 2102 1111'
+
+# Three digit number followed by space followed by two digit number
+pattern = '(\d{3}) (\d{2})'
+
+# match variable contains a Match object.
+match = re.search(pattern, string)
+
+if match:
+  print(match.group())
+else:
+  print("pattern not found")
